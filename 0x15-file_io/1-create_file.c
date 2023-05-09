e460dfcf6d4af7e56b6695b419d1af6225fdea73
root <Henok>
2023-05-08 18:01:48 -0700

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * create_file
+ * create_file: creat file
  * @filename: filename
  * @text_content: content write in the file
- * auth Henok dar
  * Return: 1 if it success: -1 if it fails
  */
 int create_file(const char *filename, char *text_content)
